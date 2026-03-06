b23676ab90625625ee4bfd6c01b2adc0d8f7fb32
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2026-03-06 10:25:51 -0300

    Splash Page
    Adição da tela de carregamento do APP (Splash Page), para tal, foi criado uma nova pasta 'UI', para seguir as novas normas do Flutter Architeture, também foi adicionado nos assets o logo da empresa, para poderser utilizado na página.
    
    O main.dart foi alterado para carregar a Splash Page (como um Widget)

---
+++ src/siderapredict/lib/ui/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFD0021B),
+              Color(0xFFFFFFFF),
+            ],
+            stops: [0.3, 0.8],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(flex: 4),
+              Image.asset(
+                'assets/soufer.png',
+                width: 300,
+                fit: BoxFit.contain,
+              ),
+              const Spacer(flex: 6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
